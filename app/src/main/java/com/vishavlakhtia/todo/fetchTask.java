3d46045ef9d05bef4c2695b3a264f30aae67186d
Vishav Lakhtia <37496498+vishav676@users.noreply.github.com>
2020-05-05 01:36:29 +0200

    retrieve data from Database

--- app/src/main/java/com/vishavlakhtia/todo/fetchTask.java
+++ app/src/main/java/com/vishavlakhtia/todo/fetchTask.java
@@ -1,6 +1,7 @@
 package com.vishavlakhtia.todo;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,33 +15,33 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class fetchTask extends ArrayAdapter<Task>{
-    TextView title;
-    private ArrayList<Task> dataSet;
-    Context mContext;
-    public fetchTask(@NonNull Context context, ArrayList<Task> data) {
+public class fetchTask extends ArrayAdapter{
+    private TextView title;
+    private ArrayList dataSet;
+    private Context mContext;
+    public fetchTask(@NonNull Context context, ArrayList data) {
         super(context, R.layout.item_list,data);
         this.dataSet = data;
         this.mContext = context;
     }
-
+    private DBHelper db = new DBHelper(getContext());
     @NonNull
     @Override
     public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        final Task task = getItem(position);
+        final Object task =  getItem(position);
         if(convertView == null){
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_list,parent,false);
             title = (TextView)convertView.findViewById(R.id.disTitle);
         }
         assert task != null;
-        title.setText(task.getTitle());
+        title.setText(task.toString());
         ImageView delete = (ImageView)convertView.findViewById(R.id.del);
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(),"delete "+ task.getTitle(),Toast.LENGTH_SHORT).show();
-                dataSet.remove(position);
+                Toast.makeText(getContext(),"delete "+ task.toString(),Toast.LENGTH_SHORT).show();
+                db.deleteTask((Integer)position);
                 notifyDataSetChanged();
             }
         });
