65781b2186325d1d26d402607d0c1e43e49f9a51
Vishav Lakhtia <37496498+vishav676@users.noreply.github.com>
2020-05-05 01:35:31 +0200

    adding task to Database

--- app/src/main/java/com/vishavlakhtia/todo/addToList.java
+++ app/src/main/java/com/vishavlakhtia/todo/addToList.java
@@ -9,17 +9,19 @@ import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class addToList extends AppCompatActivity {
 
     EditText et_title;
     EditText et_description;
     Button addNew;
+    DBHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_list);
-        
+        db = new DBHelper(this);
         et_title = (EditText)findViewById(R.id.titleEt);
         addNew = (Button)findViewById(R.id.addnewTask);
 
@@ -28,11 +30,9 @@ public class addToList extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String title = et_title.getText().toString();
-                Task task = new Task(title);
-                Intent i = new Intent();
-                i.putExtra("NEWTASK", task);
-                setResult(2,i);
-                finish();
+                db.insertTask(title);
+                Toast.makeText(getApplicationContext(),"done",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
             }
         });
     }
