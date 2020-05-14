76cc31fa1b4e01121389e618bd72b33f6822818e
Vishav Lakhtia <37496498+vishav676@users.noreply.github.com>
2020-05-14 20:55:52 +0200

    adding task to online database

--- app/src/main/java/com/vishavlakhtia/todo/addToList.java
+++ app/src/main/java/com/vishavlakhtia/todo/addToList.java
@@ -1,5 +1,6 @@
 package com.vishavlakhtia.todo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,12 +12,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
 public class addToList extends AppCompatActivity {
 
     EditText et_title;
     EditText et_description;
     Button addNew;
     DBHelper db;
+    String saveCurrentDate, saveCurrentTime;
+    String id;
+    DatabaseReference dataRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +39,31 @@ public class addToList extends AppCompatActivity {
         db = new DBHelper(this);
         et_title = (EditText)findViewById(R.id.titleEt);
         addNew = (Button)findViewById(R.id.addnewTask);
+        final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
 
         addNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String title = et_title.getText().toString();
+                Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+                saveCurrentDate = currentDate.format(calendar.getTime());
+
+                SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+                saveCurrentTime = currentTime.format(calendar.getTime());
+                id = saveCurrentDate+saveCurrentTime;
+                HashMap<String,Object> dataMap = new HashMap<>();
+                dataMap.put("title",title);
+                dataMap.put("id", id);
+                dataRef = FirebaseDatabase.getInstance().getReference();
+                dataRef.child(currentUser.getUid()).child("unComplete").child(id).updateChildren(dataMap)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(getApplicationContext(),"Task Added Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        });
                 db.insertTask(title);
                 Toast.makeText(getApplicationContext(),"done",Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
